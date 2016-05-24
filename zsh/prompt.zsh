a616bdf8f0ef137ad6e924abc2d4edd9b4440f0e
David Obwaller <david.obwaller@gmail.com>
2016-05-24 17:05:59 +0200

    color prompt red if connected via ssh

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,10 +1,20 @@
+set_PS1() {
+  env | grep SSH_CLIENT > /dev/null
+  if [ $? -eq 0 ]; then
+    # Color prompt red if connected over ssh.
+    PS1="%B%{$fg[red]%}$1%{$fg[default]%}%b"
+  else
+    PS1="$1"
+  fi
+}
+
 long() {
-  PS1="%~ $ "
+  set_PS1 "%~ $ "
   RPS1="%n@%M"
 }
 
 short() {
-  PS1="%1~ $ "
+  set_PS1 "%1~ $ "
 }
 
 long
