c8e21cb9c01d98545c4788a0fc42f67de0788cba
David Obwaller <david.obwaller@gmail.com>
2016-05-25 12:46:30 +0200

    updated link script

--- link.sh
+++ link.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 tmp=`pwd`
 cd
-ln -s $tmp/zsh .zsh
-ln -s $tmp/zsh/zshrc .zshrc
+link() {
+  if [ -f $2 -a ! -L $2 ]; then
+    echo "E: $2 is not a symlink"
+  elif [ -L $2 ]; then
+    target=`readlink $2`
+    if [ $target = $tmp/$1 ]; then
+      echo "$2 is already linked"
+    else
+      echo "$2 is linked, but points at wrong target"
+    fi
+  else
+    ln -s $tmp/$1 $2
+    echo "linked $2"
+  fi
+}
+link zsh .zsh
+link zsh/zshrc .zshrc
+link screenrc .screenrc
+link ghci .ghci
