f64f2f99ccfe7f3ebfd1ace44541498817a89ef1
David Obwaller <david.obwaller@gmail.com>
2016-05-25 15:24:24 +0200

    bind alt-left/right to backward/forward-word on Linux

--- zsh/le.zsh
+++ zsh/le.zsh
@@ -10,3 +10,10 @@ slash-backward-kill-word() {
 zle -N slash-backward-kill-word
 bindkey '\e^?' slash-backward-kill-word
 # key: alt-backspace
+
+case `uname` in
+  Linux)
+    bindkey "^[[1;3C" forward-word
+    bindkey "^[[1;3D" backward-word
+    ;;
+esac
