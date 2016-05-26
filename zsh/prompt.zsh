65068c63439162e2deb436c3365fcf48893e3ecd
David Obwaller <david.obwaller@gmail.com>
2016-05-26 13:34:31 +0200

    zsh: adopt example vcs style from info docs

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,6 +2,12 @@ autoload -Uz vcs_info
 precmd () { vcs_info }
 setopt prompt_subst
 
+zstyle ':vcs_info:*' actionformats \
+  '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f '
+zstyle ':vcs_info:*' formats \
+  '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{5}]%f '
+zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
+
 set_PS1() {
   env | grep SSH_CLIENT > /dev/null
   if [ $? -eq 0 ]; then
