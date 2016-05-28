c351475200e688452fec00485f2bc55d4b7722c7
David Obwaller <david.obwaller@gmail.com>
2016-05-28 22:36:10 +0200

    zsh/path: added MacTeX path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,2 +1,8 @@
 PATH=/bin:/usr/local/bin:/usr/bin:/sbin:/usr/local/sbin:/usr/sbin
 PATH=$HOME/bin:$HOME/.cabal/bin:$PATH
+
+case `uname` in
+  Darwin)
+    PATH=/usr/local/texlive/2015/bin/x86_64-darwin:$PATH
+    ;;
+esac
