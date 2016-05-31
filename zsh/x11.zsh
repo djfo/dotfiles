48be3530ed41f059f31d557c1d9b480af7dcde84
David Obwaller <david.obwaller@student.uibk.ac.at>
2016-05-31 17:56:08 +0200

    zsh: added mirror(), modified dual()

--- zsh/x11.zsh
+++ zsh/x11.zsh
@@ -1,5 +1,10 @@
 function dual() {
-  xrandr --output DP2 --primary --right-of eDP1 --auto
+  #xrandr --output DP2 --primary --right-of eDP1 --auto
+  xrandr --output DP2 --primary --left-of eDP1 --auto
+}
+
+function mirror() {
+  xrandr --output DP2 --primary --same-as eDP1 --auto
 }
 
 function single() {
