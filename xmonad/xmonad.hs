f9bf7ebdccebac3bb2e875adbaddb3371046a356
David Obwaller <david.obwaller@student.uibk.ac.at>
2016-06-12 15:39:27 +0200

    xmonad: cycle through keyboard layouts (M-S-Space)

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,7 +1,16 @@
 import XMonad
+import XMonad.Util.CustomKeys
 
 main :: IO ()
 main =
   xmonad def {
     terminal = "gnome-terminal"
+  , keys = customKeys delkeys inskeys
   }
+  where
+    delkeys :: XConfig l -> [(KeyMask, KeySym)]
+    delkeys XConfig {modMask = modm} =
+      [ (modm .|. shiftMask, xK_space) ]
+    inskeys :: XConfig l -> [((KeyMask, KeySym), X ())]
+    inskeys conf@(XConfig {modMask = modm}) =
+      [ ((modm .|. shiftMask, xK_space), spawn "cycle-kbd-layout" ) ]
