f70ae4a27274e07217ff075e6f233d636e799388
David Obwaller <david.obwaller@student.uibk.ac.at>
2016-06-12 11:35:25 +0200

    colored output from `link.sh`

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # dotfiles
 
-After cloning this repository
+This repository contains a bunch of configuration files I use on my machines.
+
+After cloning this repository you may want to initialize the submodules and
+install links to your home directory.
 
     $ git submodules init
     $ git submodules update
--- link.sh
+++ link.sh
@@ -1,28 +1,41 @@
 #!/bin/sh
 tmp=`pwd`
 cd
+escape() {
+  echo "[$1m$2[0m"
+}
+err() {
+  escape 31 "E: $1"
+}
+info () {
+  escape 33 "I: $1"
+}
+msg() {
+  escape 32 "$1"
+}
 link() {
   if [ -e $2 -a ! -L $2 ]; then
-    echo "E: $2 is not a symlink"
+    err echo "$2 is not a symlink"
   elif [ -L $2 ]; then
     target=`readlink $2`
     if [ $target = $tmp/$1 ]; then
-      echo "$2 is already linked"
+      info "$2 is already linked"
     else
-      echo "$2 is linked, but points at wrong target"
+      err "$2 is linked, but points at wrong target"
     fi
   else
     ln -s $tmp/$1 $2
-    echo "linked $2"
+    msg "linked $2"
   fi
 }
 md() {
   if [ -d $1 ]; then
-    echo "E: directory $1 already exists"
+    info "directory $1 already exists"
   elif [ -f $1 ]; then
-    echo "E: file $1 exists, but is not a directory"
+    err "file $1 exists, but is not a directory"
   else
     mkdir -p $1
+    msg "created directory $1"
   fi
 }
 link zsh .zsh
