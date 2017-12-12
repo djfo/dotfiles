24e870f6601cbff76135db131d55ddeed0410a79
David Obwaller <david.obwaller@student.uibk.ac.at>
2017-12-12 11:19:49 +0100

    xmonad: use `dzen2` dock; toggle struts using M-C-b

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,18 +1,28 @@
 module Main where
 
-import XMonad
-import XMonad.Util.CustomKeys -- xmonad-contrib
+import           XMonad
+import           XMonad.Hooks.DynamicLog
+import           XMonad.Hooks.ManageDocks
+import           XMonad.Util.CustomKeys
+import           XMonad.Util.Run
 
 main :: IO ()
 main =
-  xmonad def {
-    terminal = "gnome-terminal"
-  , keys = customKeys delkeys inskeys
-  }
+  do
+    h <- spawnPipe "dzen2 -dock"
+
+    xmonad $ docks def {
+      terminal = "gnome-terminal"
+    , keys = customKeys delkeys inskeys
+    , logHook = dynamicLogWithPP $ def { ppOutput = hPutStrLn h }
+    , layoutHook = avoidStruts $ layoutHook def
+    }
   where
     delkeys :: XConfig l -> [(KeyMask, KeySym)]
     delkeys XConfig {modMask = modm} =
       [ (modm .|. shiftMask, xK_space) ]
     inskeys :: XConfig l -> [((KeyMask, KeySym), X ())]
     inskeys conf@(XConfig {modMask = modm}) =
-      [ ((modm .|. shiftMask, xK_space), spawn "cycle-kbd-layout" ) ]
+      [ ((modm .|. shiftMask, xK_space), spawn "cycle-kbd-layout")
+      , ((modm .|. controlMask, xK_b), sendMessage ToggleStruts)
+      ]
