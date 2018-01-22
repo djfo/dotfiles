e78fd0e2c64822cc7f37ae7eeacb67ed87fb3cbb
David Obwaller <david.obwaller@student.uibk.ac.at>
2018-01-22 12:51:06 +0100

    xmonad-clock: show time in current timezone

--- xmonad/xmonad-clock.hs
+++ xmonad/xmonad-clock.hs
@@ -11,7 +11,12 @@ main :: IO ()
 main = do
   h <- spawnPipe "dzen2 -dock -xs 2 -x 1200 -ta r"
   void $ forever $ do
-    time <- getCurrentTime
-    let formattedTime = formatTime defaultTimeLocale "%c" time
+    formattedTime <- getFormattedTime
     hPutStrLn h formattedTime
     threadDelay $ 1000 * 1000
+
+getFormattedTime :: IO String
+getFormattedTime = do
+  tz <- getCurrentTimeZone
+  time <- utcToLocalTime tz <$> getCurrentTime
+  return $ formatTime defaultTimeLocale "%c" time
