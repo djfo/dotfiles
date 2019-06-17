bde0a0ee34d39de8191fdac1d1148dd9cd1cebf1
David Obwaller <david.obwaller@uibk.ac.at>
2019-06-17 16:29:57 +0200

    [zsh] use `xdg-open` in `yt` and `wh` commands
    - define `xdg-open` (in terms of `open`) on Darwin (macOS)

--- zsh/cmd.zsh
+++ zsh/cmd.zsh
@@ -1,9 +1,15 @@
+if darwin; then
+  xdg-open() {
+    open $*
+  }
+fi
+
 yt() {
   q=`echo $* | tr ' ' +`
-  open https://www.youtube.com/results\?search_query=$q
+  xdg-open https://www.youtube.com/results\?search_query=$q
 }
 
 wh() {
   q=`echo $* | tr ' ' +`
-  open https://www.willhaben.at/iad/kaufen-und-verkaufen/marktplatz\?keyword=$q
+  xdg-open https://www.willhaben.at/iad/kaufen-und-verkaufen/marktplatz\?keyword=$q
 }
