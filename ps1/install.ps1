029353d36f66ab1db0df56f02f4cb17fae697e00
David Obwaller <david.obwaller@gmail.com>
2020-05-18 08:09:12 -0700

    [ps1] Fixed invocation information in `install.ps1` script

--- ps1/install.ps1
+++ ps1/install.ps1
@@ -1,5 +1,9 @@
-# Run in Windows PowerShell (Admin), i.e. <Win>-X, A
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+# Run this script in Windows PowerShell (Admin) after manually enabling script execution:
+#
+# > Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
+#
+# An instance of Windows PowerShell with Admin permissions can be started using the keyboard shortcut <Win>-X, A .
+
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Install-PackageProvider -Name NuGet
 Install-Package PSReadLine -Force
