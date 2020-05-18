2feba601107534dc2bcd705cb2781f5940d3b15d
David Obwaller <david.obwaller@gmail.com>
2020-05-18 08:09:42 -0700

    [ps1] Add Git to path

--- ps1/profile.ps1
+++ ps1/profile.ps1
@@ -3,6 +3,7 @@
 if ($host.Name -eq 'ConsoleHost') {
   $env:Path += ";${env:ProgramFiles(x86)}\Vim\vim81"
   $env:Path += ";${env:ProgramFiles}\7-Zip"
+  $env:Path += ";${env:ProgramFiles}\Git\bin"
   $env:Path += ";${env:USERPROFILE}\AppData\Local\Programs\Python\Python38"
 
   Import-Module PSReadLine
