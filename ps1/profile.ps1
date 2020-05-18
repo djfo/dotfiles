db273a247c51a93643f814ca86ffb0078fde27a6
David Obwaller <david.obwaller@gmail.com>
2020-05-18 08:12:22 -0700

    [ps1] vim81 -> vim82

--- ps1/profile.ps1
+++ ps1/profile.ps1
@@ -1,7 +1,7 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 if ($host.Name -eq 'ConsoleHost') {
-  $env:Path += ";${env:ProgramFiles(x86)}\Vim\vim81"
+  $env:Path += ";${env:ProgramFiles(x86)}\Vim\vim82"
   $env:Path += ";${env:ProgramFiles}\7-Zip"
   $env:Path += ";${env:ProgramFiles}\Git\bin"
   $env:Path += ";${env:USERPROFILE}\AppData\Local\Programs\Python\Python38"
