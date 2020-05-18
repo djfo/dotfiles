b6a4d7e1f582c4aa38b317f5ec63df100cdab131
David Obwaller <david.obwaller@gmail.com>
2020-05-18 10:45:35 +0200

    [ps1] Add path to Python interpreter to environment

--- ps1/profile.ps1
+++ ps1/profile.ps1
@@ -3,6 +3,7 @@
 if ($host.Name -eq 'ConsoleHost') {
   $env:Path += ";${env:ProgramFiles(x86)}\Vim\vim81"
   $env:Path += ";${env:ProgramFiles}\7-Zip"
+  $env:Path += ";${env:USERPROFILE}\AppData\Local\Programs\Python\Python38"
 
   Import-Module PSReadLine
   Set-PSReadLineOption -EditMode Emacs
