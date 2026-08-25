149cf15efe14c15707f09a1bbd56e11a7cb72305
David Obwaller <d.obwaller@runbox.com>
2026-08-25 12:00:13 +0200

    zsh: Remove unused aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,69 +1,51 @@
 for cmd in rm cp mv; alias $cmd="$cmd -i"
 
-alias ...="cd ../.."
-alias ..="cd .."
-alias d-c="docker-compose"
-alias d="darcs"
-alias dh="dirs -v"
 alias g="git"
-alias gim="git -c core.fileMode=false"
-alias h="history"
-alias lx="exa -l --git"
-alias pd="popd"
-alias ts="moreutils-ts"
-alias vi="vim"
+alias k="kubectl"
 
 if linux; then
-  alias ls="ls -F1 --color=auto"
+  alias du="du --si"
   alias lc="ls -FC --color=auto"
   alias ll="ls -Fl --color=auto"
-
-  alias du="du --si"
+  alias ls="ls -F1 --color=auto"
 fi
 
 if bsd; then
-  alias ls="ls -GF1"
+  alias du="du -h"
   alias lc="ls -GFC"
   alias ll="ls -GFlh"
-  alias grep="grep --color=auto"
-  alias du="du -h"
+  alias ls="ls -GF1"
 fi
 
 if darwin; then
-  alias e="mvim"
+  alias gvim="mvim"
   alias o="open"
   alias r="open -R"
-
-  alias c="pbcopy"
-  alias p="pbpaste"
+  alias say.de="say -v Anna"
+  alias say.en_US="say"
+  alias say.fr_CA="say -v Amélie"
 
   function man_() {
     man $* | col -b
   }
 
-  alias gvim="mvim"
-
-  alias say.de="say -v Anna"
-  alias say.en_US="say"
-  alias say.fr_CA="say -v Amélie"
-
   case `uname -m` in
     arm64)
       alias brew.x64_86=/usr/local/bin/brew
+      PATH=/opt/homebrew/bin:$PATH
       ;;
   esac
 fi
 
 if linux || freebsd; then
-  alias xlock="xlock -mode rain"
-  alias pdfopen="pdfopen --viewer xpdf"
-  alias xclock="xclock -geometry 100x100-40+40"
-
   function my_open() {
     xdg-open $1 >/dev/null 2>/dev/null &
     disown $!
   }
-  alias o="my_open"
 
   alias mvim="gvim"
+  alias o="my_open"
+  alias pdfopen="pdfopen --viewer xpdf"
+  alias xclock="xclock -geometry 100x100-40+40"
+  alias xlock="xlock -mode rain"
 fi
